5adef0bd20af3572f717aaeaf0992eebd770bc6b
Joseph Spear <51432157+jspear16@users.noreply.github.com>
2021-08-21 20:06:44 -0500

    Add files via upload

---
+++ Embedded_Systems_Work/SETUP.sh
@@ -0,0 +1,149 @@
+# Joseph Spear 9 July 2021
+#
+# This is the Setup file for the SIOS-C 11B Project. All prerequisite installations are downloaded and installed with this script.
+# Make sure to give this file execution privilages with chmod +x SETUP.sh
+ 
+
+
+function show_help(){
+    echo -e "Usage: $0 [option]\n"
+    echo -e "\n"
+
+    echo -e "-------------------------------------------------------------------------------------------------"
+    echo -e "|Default (no user-given arguments) setup script installs the following:                          |"
+    echo -e "|                                                                                                |"
+    echo -e "|  arm-none-eabi-*, all program files related to compiling code for the ARM Cortex-M processor   |"
+    echo -e "|  The miktex distribution of latex                                                              |"
+    echo -e "|  The doxygen code documentation application                                                    |"
+    echo -e "|  The doxygen-gui for more use of doxygen's graphical capabilities                              |"
+    echo -e "|  The graphviz application to support the doxygen-gui                                           |"
+    echo -e "-------------------------------------------------------------------------------------------------\n"
+    echo -e "\n"
+
+    echo -e "------------------------------------------------------------------------------"
+    echo -e "|Options:                                                                     |"
+    echo -e "|                                                                             |"
+    echo -e "|  -h|--help, Print this help menu                                            |"
+    echo -e "|  -U|--uninstall-all, Uninstall all programs installed from a default setup  |"
+    echo -e "|  --uninstall-arm, Uninstalls the current arm-none-eabi-* programs only      |"
+    echo -e "|  --uninstall-mik, Uninstalls the miktex distribution                        |"
+    echo -e "|  --uninstall-dox, Uninstalls doxygen and supporting programs                |"
+    echo -e "------------------------------------------------------------------------------\n"
+    echo -e "\n"
+    
+    return 0
+}
+
+
+case "${1}" in
+
+    "")
+
+        echo -e "------------------------------------------\n| Setup for SIOS-C 11B is now beginning  |\n------------------------------------------\n"
+
+        # TODO Uninstall any possible ARM compiler
+
+        #sudo apt-get --purge remove 
+
+        #   Get CPU architecture
+        CPU_ARCH_TYEPE=$(uname -m)
+
+        #   Compiler name variables
+        TARBALL_NAME="gnu-arm-embedded-toolchain.tar.xz"
+        COMPILER_NAME=gnu-arm-embedded-toolchain
+
+
+        # Check Arch type
+        if [ ${CPU_ARCH_TYEPE} = "x86_64" ]
+        then
+            echo "You are running a ${CPU_ARCH_TYEPE} system."
+        else
+            echo "This is not an x86_64 computer"
+            exit -1
+        fi
+
+        # Moving compiler to proper place
+        if [ -d "/usr/bin/gnu-arm-embedded-toolchain" ]; then
+            echo -e "\nThe arm compiler has already been downloaded\n"
+        else
+            echo -e "Extracting Tarball now...\n"
+            sudo cp ./Compiler_Tarball/${TARBALL_NAME} /usr/bin/
+            cd /usr/bin/
+            sudo tar -xf ${TARBALL_NAME}
+            sudo rm -r ${TARBALL_NAME}
+            cd -
+        fi
+
+
+        # Permanantly add the Path
+        if [[ $PATH == *"/usr/bin/gnu-arm-embedded-toolchain/bin/"* ]]; then 
+            echo -e "\n/usr/bin/gnu-arm-embedded-toolchain/bin/ is already added to your PATH \n"
+        else
+            echo >> ${HOME}/.bashrc
+            echo "# GCC ARM Compiler was added to the PATH here" >> $HOME/.bashrc
+            echo "PATH=${PATH}:/usr/bin/gnu-arm-embedded-toolchain/bin/" >> $HOME/.bashrc
+            echo -e "\nCompiler directory added to global PATH\n"
+        fi
+        echo -e "\narm-none-eabi files were installed successfully\n"
+
+
+        # Install Miktex, Doxygen, and Doxygen support
+        sudo apt-get install miktex
+        sudo apt-get install doxygen 
+        sudo apt-get install doxygen-gui
+        sudo apt-get install graphviz
+
+
+        # TODO Add a way to install necessary LaTex packages
+
+        echo -e "Finished installing necessary files for MAD IMU\n"
+
+        exit 0
+        # ---------------------------------------- end of default case ----------------------------------------
+        ;;
+
+    --uninstall-all|-U)
+        echo -e "Uninstalling all programs\n"
+
+
+        # Move to proper directory
+        cd /usr/bin/
+
+        sudo rm -r gnu-arm-embedded-toolchain
+        sudo apt-get --purge remove miktex
+        sudo apt-get --purge remove doxygen 
+        sudo apt-get --purge remove doxygen-gui
+        sudo apt-get --purge remove graphviz
+        echo -e "Finished uninstall\n"
+        ;;
+    
+    --uninstall-arm)
+        echo -e "Uninstalling all arm-none-eabi-* files\n"
+        # Move to proper directory
+        cd /usr/bin/
+        
+        sudo rm -r gnu-arm-embedded-toolchain
+        echo -e "Finished uninstall\n"
+        ;;
+    
+    --uninstall-mik)
+        echo -e "Uninstalling miktex\n"
+        sudo apt-get --purge remove miktex
+        echo -e "Finished uninstall\n"
+        ;;
+    
+    --uninstall-dox)
+        echo -e "Uninstalling doxygen and supporting files\n"
+
+        sudo apt-get --purge remove doxygen 
+        sudo apt-get --purge remove doxygen-gui
+        sudo apt-get --purge remove graphviz
+        echo -e "Finished uninstall\n"
+        ;;
+    
+    --help|-h|*)
+        show_help;;
+
+esac
+
+#   end of script
---
+++ Embedded_Systems_Work/audioPlotting.sh
@@ -0,0 +1,462 @@
+# Joseph Spear 8/19/21
+#
+# This file will take in information based on the SIOS 11B Data Transmission API, parse the important sound data,
+# convert the hex string into a base 10 integer, and then plot them using gnuplot
+
+
+
+if [[ $1 == "serial_"* ]];then
+
+    # Assigns variables for the I/O Arguments
+    INPUT_FILE=$1
+
+    # cat's the Input file and then searches for the timestamp (first line with a size of exactly 9)
+    LINES=`cat $INPUT_FILE`
+    for LINE in $LINES
+    do
+        LENGTH=`expr length "$LINE"`
+        if [[ $LENGTH == 9 ]];then
+        TIMESTAMP=${LINE:0:8}
+        break
+        fi
+    done
+
+    # Makes the new Directory and output file
+    DIRECTORY_NAME=Data_$TIMESTAMP
+    mkdir $DIRECTORY_NAME
+    OUTPUT_FILE=./$DIRECTORY_NAME/Raw_Data_$TIMESTAMP.txt
+    touch $OUTPUT_FILE
+
+
+    # Cut all of the data to be 4 characters long, starting at the first character of data (OUTPUT_FILE is actinga as a temp file here because it will be rewritten later)
+    cat $INPUT_FILE | cut -c  14-17 | cat > $OUTPUT_FILE
+
+
+    # Variable to help parse the data
+    LINES=`cat $OUTPUT_FILE`
+
+    # Rewrite OUTPUT_FILE with the very first line being the Timestamp
+    echo "$TIMESTAMP" > "$OUTPUT_FILE"
+
+    # For loop to go through the LINES set, writing proper data to OUTPUT_FILE, and ignoring improper data
+    for LINE in $LINES
+    do
+        VAR=${LINE:0:1}
+
+        LENGTH=`expr length "$LINE"`
+        if [[ "$VAR" == '~' ]]; then
+            echo ''
+        elif [[ $LENGTH != 4 ]]; then
+            echo ''
+        else
+            echo "$LINE" >> "$OUTPUT_FILE"
+        fi
+    done
+
+    # Remove the second line and extra data from OUTPUT_FILE. It is a repeat of the Timestamp
+    sed -i '2d' $OUTPUT_FILE
+    sed -i '3202,$ d' $OUTPUT_FILE
+
+    # File used to store Base 10 values
+    DECIMAL_FILE=./$DIRECTORY_NAME/Raw_Data_inDecimal_$TIMESTAMP.txt
+    touch $DECIMAL_FILE
+
+    # Puts all of the data from Output into the Base 10 file, excluding the timestamp. Just sound data
+    sed '1d' $OUTPUT_FILE > $DECIMAL_FILE
+
+    # Variable to help parse the data
+    LINES=`cat $DECIMAL_FILE`
+
+    # Erase all data on the file to write anew
+    echo > $DECIMAL_FILE
+
+    # Actual conversion from Hex to Base 10 occurs here
+    for LINE in $LINES
+    do
+        CHAR1=${LINE:0:1}
+        case $CHAR1 in
+            "0") declare -i NUM1 && NUM1=0;;
+            "1") declare -i NUM1 && NUM1=1;;
+            "2") declare -i NUM1 && NUM1=2;;
+            "3") declare -i NUM1 && NUM1=3;;
+            "4") declare -i NUM1 && NUM1=4;;
+            "5") declare -i NUM1 && NUM1=5;;
+            "6") declare -i NUM1 && NUM1=6;;
+            "7") declare -i NUM1 && NUM1=7;;
+            "8") declare -i NUM1 && NUM1=8;;
+            "9") declare -i NUM1 && NUM1=9;;
+            "a") declare -i NUM1 && NUM1=10;;
+            "b") declare -i NUM1 && NUM1=11;;
+            "c") declare -i NUM1 && NUM1=12;;
+            "d") declare -i NUM1 && NUM1=13;;
+            "e") declare -i NUM1 && NUM1=14;;
+            "f") declare -i NUM1 && NUM1=15;;
+        esac
+        
+        NUM1=$NUM1*16*16*16
+        
+
+
+        CHAR2=${LINE:1:1}
+            case $CHAR2 in
+            "0") declare -i NUM2 && NUM2=0;;
+            "1") declare -i NUM2 && NUM2=1;;
+            "2") declare -i NUM2 && NUM2=2;;
+            "3") declare -i NUM2 && NUM2=3;;
+            "4") declare -i NUM2 && NUM2=4;;
+            "5") declare -i NUM2 && NUM2=5;;
+            "6") declare -i NUM2 && NUM2=6;;
+            "7") declare -i NUM2 && NUM2=7;;
+            "8") declare -i NUM2 && NUM2=8;;
+            "9") declare -i NUM2 && NUM2=9;;
+            "a") declare -i NUM2 && NUM2=10;;
+            "b") declare -i NUM2 && NUM2=11;;
+            "c") declare -i NUM2 && NUM2=12;;
+            "d") declare -i NUM2 && NUM2=13;;
+            "e") declare -i NUM2 && NUM2=14;;
+            "f") declare -i NUM2 && NUM2=15;;
+        esac
+        
+        NUM2=$NUM2*16*16
+        
+
+        CHAR3=${LINE:2:1}
+            case $CHAR3 in
+            "0") declare -i NUM3 && NUM3=0;;
+            "1") declare -i NUM3 && NUM3=1;;
+            "2") declare -i NUM3 && NUM3=2;;
+            "3") declare -i NUM3 && NUM3=3;;
+            "4") declare -i NUM3 && NUM3=4;;
+            "5") declare -i NUM3 && NUM3=5;;
+            "6") declare -i NUM3 && NUM3=6;;
+            "7") declare -i NUM3 && NUM3=7;;
+            "8") declare -i NUM3 && NUM3=8;;
+            "9") declare -i NUM3 && NUM3=9;;
+            "a") declare -i NUM3 && NUM3=10;;
+            "b") declare -i NUM3 && NUM3=11;;
+            "c") declare -i NUM3 && NUM3=12;;
+            "d") declare -i NUM3 && NUM3=13;;
+            "e") declare -i NUM3 && NUM3=14;;
+            "f") declare -i NUM3 && NUM3=15;;
+        esac
+        
+        NUM3=$NUM3*16
+
+
+        CHAR4=${LINE:3:1}
+        case $CHAR4 in
+            "0") declare -i NUM4 && NUM4=0;;
+            "1") declare -i NUM4 && NUM4=1;;
+            "2") declare -i NUM4 && NUM4=2;;
+            "3") declare -i NUM4 && NUM4=3;;
+            "4") declare -i NUM4 && NUM4=4;;
+            "5") declare -i NUM4 && NUM4=5;;
+            "6") declare -i NUM4 && NUM4=6;;
+            "7") declare -i NUM4 && NUM4=7;;
+            "8") declare -i NUM4 && NUM4=8;;
+            "9") declare -i NUM4 && NUM4=9;;
+            "a") declare -i NUM4 && NUM4=10;;
+            "b") declare -i NUM4 && NUM4=11;;
+            "c") declare -i NUM4 && NUM4=12;;
+            "d") declare -i NUM4 && NUM4=13;;
+            "e") declare -i NUM4 && NUM4=14;;
+            "f") declare -i NUM4 && NUM4=15;;
+        esac
+        
+        NUM4=$NUM4
+
+        # The following few lines make sure the data comes in between -32767 and +32767. Range is initially between 0 and 65536
+        declare -i TOTAL
+        declare -i TEMP
+        declare -i TOPVALUE
+        TOPVALUE=32766
+        TEMP=$NUM1+$NUM2+$NUM3+$NUM4
+        echo $TEMP
+
+        if [[ $TEMP -ge $TOPVALUE ]]; then
+            TOTAL=($TEMP-65536)
+        else
+            TOTAL=$TEMP
+        fi
+
+        echo $TOTAL
+        echo ''
+        # Here the calculated value is appended to the Base 10 file
+        echo $TOTAL >> $DECIMAL_FILE
+
+    done
+
+    # Remove the initial blank line
+    sed -i "1d" $DECIMAL_FILE
+
+    # Variable to gather the proper file name
+    NAME="SOUND_GRAPH_$TIMESTAMP"
+
+    # Gnuplot execution to generate a .png file
+    /usr/bin/gnuplot -e "set terminal png; set title 'Data Acquisition. Timestamp: $TIMESTAMP'; set output '$DIRECTORY_NAME/$NAME.png'; \
+    set xlabel 'Sample Number'; set ylabel 'Sound Amplitude'; plot '~/Desktop/AudioPlotting/$DECIMAL_FILE' smooth bezier title '$TIMESTAMP'"
+
+    # Copy the file into the parent directory. To be used when creating All_Plot.png
+    cp Data_*/Raw_Data_inDecimal_*.txt ./
+
+# End of the If case
+
+
+
+
+
+
+
+
+elif [[ $1 == "-a" ]];then
+    
+# Assigns variables for the I/O Arguments
+    INPUT_FILES=serial_*.txt
+
+    # This loop goes through all matching files, and performs the same operation as the initial case for this if statement
+    for INPUT_FILE in $INPUT_FILES
+    do
+        # cat's the Input file and then searches for the timestamp (first line with a size of exactly 9)
+        LINES=`cat $INPUT_FILE`
+        for LINE in $LINES
+        do
+            LENGTH=`expr length "$LINE"`
+            if [[ $LENGTH == 9 ]];then
+            TIMESTAMP=${LINE:0:8}
+            break
+            fi
+        done
+
+        DIRECTORY_NAME=Data_$TIMESTAMP
+        mkdir $DIRECTORY_NAME
+
+
+        OUTPUT_FILE=./$DIRECTORY_NAME/Raw_Data_$TIMESTAMP.txt
+        touch $OUTPUT_FILE
+
+
+        # Cut all of the data to be 4 characters long, starting at the first character of data (OUTPUT_FILE is actinga as a temp file here because it will be rewritten later)
+        cat $INPUT_FILE | cut -c  14-17 | cat > $OUTPUT_FILE
+
+        LINES=`cat $OUTPUT_FILE`
+
+        # Rewrite OUTPUT_FILE with the very first line being the Timestamp
+        echo "$TIMESTAMP" > "$OUTPUT_FILE"
+
+        # For loop to go through the LINES set, writing proper data to OUTPUT_FILE, and ignoring improper data
+        for LINE in $LINES
+        do
+            VAR=${LINE:0:1}
+
+            LENGTH=`expr length "$LINE"`
+            if [[ "$VAR" == '~' ]]; then
+                echo ''
+            elif [[ $LENGTH != 4 ]]; then
+                echo ''
+            else
+                echo "$LINE" >> "$OUTPUT_FILE"
+            fi
+        done
+
+    # Remove the second line and extra data from OUTPUT_FILE. It is a repeat of the Timestamp
+    sed -i '2d' $OUTPUT_FILE
+    sed -i '3202,$ d' $OUTPUT_FILE
+
+    # File used to store Base 10 values
+    DECIMAL_FILE=./$DIRECTORY_NAME/Raw_Data_inDecimal_$TIMESTAMP.txt
+    touch $DECIMAL_FILE
+
+    # Puts all of the data from Output into the Base 10 file, excluding the timestamp. Just sound data
+    sed '1d' $OUTPUT_FILE > $DECIMAL_FILE
+
+    # Variable to help parse the data
+    LINES=`cat $DECIMAL_FILE`
+
+    # Erase all data on the file to write anew
+    echo > $DECIMAL_FILE
+
+    # Actual conversion from Hex to Base 10 occurs here
+    for LINE in $LINES
+    do
+        CHAR1=${LINE:0:1}
+        case $CHAR1 in
+            "0") declare -i NUM1 && NUM1=0;;
+            "1") declare -i NUM1 && NUM1=1;;
+            "2") declare -i NUM1 && NUM1=2;;
+            "3") declare -i NUM1 && NUM1=3;;
+            "4") declare -i NUM1 && NUM1=4;;
+            "5") declare -i NUM1 && NUM1=5;;
+            "6") declare -i NUM1 && NUM1=6;;
+            "7") declare -i NUM1 && NUM1=7;;
+            "8") declare -i NUM1 && NUM1=8;;
+            "9") declare -i NUM1 && NUM1=9;;
+            "a") declare -i NUM1 && NUM1=10;;
+            "b") declare -i NUM1 && NUM1=11;;
+            "c") declare -i NUM1 && NUM1=12;;
+            "d") declare -i NUM1 && NUM1=13;;
+            "e") declare -i NUM1 && NUM1=14;;
+            "f") declare -i NUM1 && NUM1=15;;
+        esac
+        
+        NUM1=$NUM1*16*16*16
+
+
+        CHAR2=${LINE:1:1}
+            case $CHAR2 in
+            "0") declare -i NUM2 && NUM2=0;;
+            "1") declare -i NUM2 && NUM2=1;;
+            "2") declare -i NUM2 && NUM2=2;;
+            "3") declare -i NUM2 && NUM2=3;;
+            "4") declare -i NUM2 && NUM2=4;;
+            "5") declare -i NUM2 && NUM2=5;;
+            "6") declare -i NUM2 && NUM2=6;;
+            "7") declare -i NUM2 && NUM2=7;;
+            "8") declare -i NUM2 && NUM2=8;;
+            "9") declare -i NUM2 && NUM2=9;;
+            "a") declare -i NUM2 && NUM2=10;;
+            "b") declare -i NUM2 && NUM2=11;;
+            "c") declare -i NUM2 && NUM2=12;;
+            "d") declare -i NUM2 && NUM2=13;;
+            "e") declare -i NUM2 && NUM2=14;;
+            "f") declare -i NUM2 && NUM2=15;;
+        esac
+        
+        NUM2=$NUM2*16*16
+
+
+        CHAR3=${LINE:2:1}
+            case $CHAR3 in
+            "0") declare -i NUM3 && NUM3=0;;
+            "1") declare -i NUM3 && NUM3=1;;
+            "2") declare -i NUM3 && NUM3=2;;
+            "3") declare -i NUM3 && NUM3=3;;
+            "4") declare -i NUM3 && NUM3=4;;
+            "5") declare -i NUM3 && NUM3=5;;
+            "6") declare -i NUM3 && NUM3=6;;
+            "7") declare -i NUM3 && NUM3=7;;
+            "8") declare -i NUM3 && NUM3=8;;
+            "9") declare -i NUM3 && NUM3=9;;
+            "a") declare -i NUM3 && NUM3=10;;
+            "b") declare -i NUM3 && NUM3=11;;
+            "c") declare -i NUM3 && NUM3=12;;
+            "d") declare -i NUM3 && NUM3=13;;
+            "e") declare -i NUM3 && NUM3=14;;
+            "f") declare -i NUM3 && NUM3=15;;
+        esac
+        
+        NUM3=$NUM3*16
+
+
+        CHAR4=${LINE:3:1}
+        case $CHAR4 in
+            "0") declare -i NUM4 && NUM4=0;;
+            "1") declare -i NUM4 && NUM4=1;;
+            "2") declare -i NUM4 && NUM4=2;;
+            "3") declare -i NUM4 && NUM4=3;;
+            "4") declare -i NUM4 && NUM4=4;;
+            "5") declare -i NUM4 && NUM4=5;;
+            "6") declare -i NUM4 && NUM4=6;;
+            "7") declare -i NUM4 && NUM4=7;;
+            "8") declare -i NUM4 && NUM4=8;;
+            "9") declare -i NUM4 && NUM4=9;;
+            "a") declare -i NUM4 && NUM4=10;;
+            "b") declare -i NUM4 && NUM4=11;;
+            "c") declare -i NUM4 && NUM4=12;;
+            "d") declare -i NUM4 && NUM4=13;;
+            "e") declare -i NUM4 && NUM4=14;;
+            "f") declare -i NUM4 && NUM4=15;;
+        esac
+        
+        NUM4=$NUM4
+
+
+        # The following few lines make sure the data comes in between -32767 and +32767. Range is initially between 0 and 65536
+        declare -i TOTAL
+        declare -i TEMP
+        declare -i TOPVALUE
+        TOPVALUE=32766
+        TEMP=$NUM1+$NUM2+$NUM3+$NUM4
+        echo $TEMP
+
+        if [[ $TEMP -ge $TOPVALUE ]]; then
+            TOTAL=($TEMP-65536)
+        else
+            TOTAL=$TEMP
+        fi
+
+        # Here the calculated value is appended to the Base 10 file
+        echo $TOTAL >> $DECIMAL_FILE
+
+    done
+
+    # Removing the inital blank line in the Base 10 file
+    sed -i "1d" $DECIMAL_FILE
+
+    # Variable to gather the proper file name
+    NAME="SOUND_GRAPH_$TIMESTAMP"
+
+    # Gnuplot execution to generate a .png file of all the data in each of their folders
+    /usr/bin/gnuplot -e "set terminal png; set title 'Data Acquisition. Timestamp: $TIMESTAMP'; set output '$DIRECTORY_NAME/$NAME.png'; \
+    set xlabel 'Sample Number'; set ylabel 'Sound Amplitude'; plot '~/Desktop/AudioPlotting/$DECIMAL_FILE' smooth bezier title '$TIMESTAMP'"
+
+    # Copy the file into the parent directory. To be used when creating All_Plot.png
+    cp Data_*/Raw_Data_inDecimal_*.txt ./
+
+done
+
+# End of the If case
+
+
+
+
+
+
+
+
+
+elif [[ $1 == "-p" || $1 == "" ]];then
+
+    # Sets up variables to be used when generating All_Plot.png
+    NAME=All_Plot
+    RAW_FILES=( Raw_Data_inDecimal_* ) 
+    NUM_FILES=${#RAW_FILES[@]}
+    
+    # For loop to rename all the files. This makes it easier to make a for loop in the gnuplot call
+    for(( i=0; i < $NUM_FILES; i++ ))
+    do
+        declare -i j
+        j=$i+1
+        mv -u ${RAW_FILES[i]} Raw_Data_inDecimal_$j.txt
+    done
+ 
+    # Generate All_Plot.png from all the Raw Base 10 files currently in this directory
+    /usr/bin/gnuplot -e "set terminal png; set key off; set title 'Data Acquisition. All Data'; set output '$NAME.png'; \
+    set xlabel 'Sample Number'; set ylabel 'Sound Amplitude'; plot for[i = 1:$NUM_FILES] 'Raw_Data_inDecimal_'.i.'.txt' smooth bezier title ''.i.''"
+
+    # Remove all of the Raw files in this directory
+    rm Raw_Data_inDecimal_*.txt
+
+# End of the If case
+
+
+
+
+
+
+
+
+
+else # Help case
+    echo -e "Usage: $0 [optional <file>] [flag]\n"
+    echo -e "\n"
+
+    echo -e "------------------------------------------------------------------------------"
+    echo -e "|Options:                                                                     |"
+    echo -e "|                                                                             |"
+    echo -e "|  -h, Print this help menu.                                                  |"
+    echo -e "|  -p, Plot all of the Raw data .txt files on one plot.                       |"
+    echo -e "|  -a, Perform the conversion and mkdir with all initial .txt files.          |"
+    echo -e "------------------------------------------------------------------------------\n"
+    echo -e "\n"
+
+fi
